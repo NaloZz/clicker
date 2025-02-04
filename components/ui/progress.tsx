75fb0ed4ffd113d53184206bb90ad6eae37a092e
NaloZz <64098870+NaloZz@users.noreply.github.com>
2025-02-04 17:56:44 +0500

    Update progress.tsx

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -18,7 +18,7 @@ const Progress = React.forwardRef<
 		{...props}
 	>
 		<ProgressPrimitive.Indicator
-			className="h-full w-full flex-1 bg-gradient-to-r from-[#FF0000] to-white/80 "
+			className="h-full w-full flex-1 bg-gradient-to-r from-[#FFD700] to-white/80 "
 			style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
 		/>
 	</ProgressPrimitive.Root>
