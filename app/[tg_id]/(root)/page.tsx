6375953fed85abe892bf540904ad3a06fde3a4be
NaloZz <64098870+NaloZz@users.noreply.github.com>
2025-02-04 18:46:26 +0500

    Update page.tsx

--- app/[tg_id]/(root)/page.tsx
+++ app/[tg_id]/(root)/page.tsx
@@ -37,7 +37,7 @@ export default async function Home({
 				<div className="flex flex-col gap-5 self-stretch mt-auto w-[calc(100vw-32px)] fixed bottom-8 z-10">
 					<Nav />
 
- 				<div className="absolute bottom-24 left-1/2 transform -translate-x-1/2 text-white text-2xl font-extrabold">
+ 				<div className="absolute bottom-54 left-1/2 transform -translate-x-1/2 text-white text-2xl font-extrabold">
         				BURN IT ALL 🔥
     				</div>
 
