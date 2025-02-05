7c7276bca9c822f287c6072049be73a5b0e4a2b7
NaloZz <64098870+NaloZz@users.noreply.github.com>
2025-02-05 20:46:44 +0500

    Update page.tsx

--- app/[tg_id]/(root)/page.tsx
+++ app/[tg_id]/(root)/page.tsx
@@ -25,34 +25,44 @@ export default async function Home({
 		const clan = await getClan({ tg_id: user.Clan?.tg_id });
 
 		return (
-			<main className="min-h-screen flex flex-col items-center p-4 pt-2 overflow-x-hidden relative">
-				<Clan tg_id={tg_id} clan={clan} clanId={user.clanId} />
-
-				<div className="flex flex-col gap-8 items-center w-full relative z-10 mt-8">
-					<Score user={user} />
-
-					<Coin user={user} />
-				</div>
-
-				<div className="flex flex-col gap-5 self-stretch mt-auto w-[calc(100vw-32px)] fixed bottom-8 z-10">
-					<Nav />
-
-					<EnergyBar user={user} />
-				</div>
-
-				<div
-					className={cn(
-						"fixed bottom-0 left-0 w-screen h-screen z-0",
-						"[background:radial-gradient(ellipse_100%_80%_at_bottom,var(--app-color),transparent_100%)]",
-					)}
+			<div className="relative w-full h-full">
+				{/* Задний фон с GIF */}
+				<img
+					src="https://example.com/whattapcoinmin.gif" // Замените на свой URL
+					alt="Background GIF"
+					className="absolute top-0 left-0 w-full h-full object-cover blur-sm opacity-50"
 				/>
 
-				<Shop user={user} />
+				{/* Основной контент страницы */}
+				<main className="min-h-screen flex flex-col items-center p-4 pt-2 overflow-x-hidden relative z-10">
+					<Clan tg_id={tg_id} clan={clan} clanId={user.clanId} />
 
-				<Friends tg_id={tg_id} friends={user.referrals} />
+					<div className="flex flex-col gap-8 items-center w-full relative z-10 mt-8">
+						<Score user={user} />
 
-				<Missions user={user} referralsNumber={user.referrals.length} />
-			</main>
+						<Coin user={user} />
+					</div>
+
+					<div className="flex flex-col gap-5 self-stretch mt-auto w-[calc(100vw-32px)] fixed bottom-8 z-10">
+						<Nav />
+
+						<EnergyBar user={user} />
+					</div>
+
+					<div
+						className={cn(
+							"fixed bottom-0 left-0 w-screen h-screen z-0",
+							"[background:radial-gradient(ellipse_100%_80%_at_bottom,var(--app-color),transparent_100%)]",
+						)}
+					/>
+
+					<Shop user={user} />
+
+					<Friends tg_id={tg_id} friends={user.referrals} />
+
+					<Missions user={user} referralsNumber={user.referrals.length} />
+				</main>
+			</div>
 		);
 	} catch (error) {
 		console.log(error);
