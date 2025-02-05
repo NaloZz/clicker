1e5b13c37f82357504b5abfd3f0bb6e4e2b7b417
NaloZz <64098870+NaloZz@users.noreply.github.com>
2025-02-05 19:36:04 +0500

    Update page.tsx

--- app/[tg_id]/(root)/page.tsx
+++ app/[tg_id]/(root)/page.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 import prismadb from "@/lib/prismadb";
 import { getClan } from "@/lib/server-actions";
 
-import CreateButton from "@/components/ui/CreateButton";  // Используйте правильный путь в зависимости от настроек проекта
+import CreateButton from "@/components/ui/CreateButton";
 
 import Shop from "./components/shop";
 import Nav from "./components/main/Nav";
@@ -14,61 +14,53 @@ import Missions from "./components/missions";
 import EnergyBar from "./components/main/EnergyBar";
 
 export default async function Home({
-  params: { tg_id },
+	params: { tg_id },
 }: { params: { tg_id: string } }) {
-  try {
-    const user = await prismadb.user.findUniqueOrThrow({
-      where: { tg_id },
-      include: { Clan: true, referrals: true },
-    });
+	try {
+		const user = await prismadb.user.findUniqueOrThrow({
+			where: { tg_id },
+			include: { Clan: true, referrals: true },
+		});
 
-    const clan = await getClan({ tg_id: user.Clan?.tg_id });
+		const clan = await getClan({ tg_id: user.Clan?.tg_id });
 
-    return (
-      <main className="min-h-screen flex flex-col items-center p-4 pt-2 overflow-x-hidden relative">
-        <Clan tg_id={tg_id} clan={clan} clanId={user.clanId} />
+		return (
+			<main className="min-h-screen flex flex-col items-center p-4 pt-2 overflow-x-hidden relative">
+				<Clan tg_id={tg_id} clan={clan} clanId={user.clanId} />
 
-        <div className="flex flex-col gap-8 items-center w-full relative z-10 mt-8">
-          <Score user={user} />
+				<div className="flex flex-col gap-8 items-center w-full relative z-10 mt-8">
+					<Score user={user} />
 
-          <Coin user={user} />
-        </div>
+					<Coin user={user} />
+				</div>
 
-        <div className="flex flex-col gap-5 self-stretch mt-auto w-[calc(100vw-32px)] fixed bottom-8 z-10">
-          <Nav />
+				<div className="flex flex-col gap-5 self-stretch mt-auto w-[calc(100vw-32px)] fixed bottom-8 z-10">
+					<Nav />
 
-          {/* 🔥 Текст "BURN IT ALL" */}
-          <div className="absolute bottom-30 left-1/2 transform -translate-x-1/2 text-white text-2xl font-extrabold z-20">
-            BURN IT ALL
-          </div>
+					<EnergyBar user={user} />
+				</div>
 
-          {/* 🔥 Размытие + картинка огня */}
-          <div className="absolute bottom-0 left-0 w-full h-full bg-opacity-50">
-            <img 
-              src="/whattapcoinmin.gif" 
-              alt="Fire Background" 
-              className="w-full h-full object-cover blur-lg opacity-80"
-            />
-          </div>
+				<div
+					className={cn(
+						"fixed bottom-0 left-0 w-screen h-screen z-0",
+						"[background:radial-gradient(ellipse_100%_80%_at_bottom,var(--app-color),transparent_100%)]",
+					)}
+				/>
 
-          {/* 🔥 Радужный градиентный фон убран, оставлена только гифка с огнем */}
-          <div
-            className={cn(
-              "fixed bottom-0 left-0 w-screen h-screen z-0"
-            )}
-          />
+				<Shop user={user} />
 
-        </div> {/* конец блока с основным контентом */}
+				<Friends tg_id={tg_id} friends={user.referrals} />
 
-      </main>
-    );
-  } catch (error) {
-    console.log(error);
+				<Missions user={user} referralsNumber={user.referrals.length} />
+			</main>
+		);
+	} catch (error) {
+		console.log(error);
 
-    return (
-      <main className="min-h-screen flex items-center justify-center">
-        <CreateButton />
-      </main>
-    );
-  }
+		return (
+			<main className="min-h-screen flex items-center justify-center">
+				<CreateButton />
+			</main>
+		);
+	}
 }
