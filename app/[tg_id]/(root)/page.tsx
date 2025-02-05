6a3f000751173576cb4a292059989d4e7ce424cf
NaloZz <64098870+NaloZz@users.noreply.github.com>
2025-02-05 19:29:09 +0500

    Update page.tsx

--- app/[tg_id]/(root)/page.tsx
+++ app/[tg_id]/(root)/page.tsx
@@ -29,28 +29,39 @@ export default async function Home({
         <Clan tg_id={tg_id} clan={clan} clanId={user.clanId} />
 
         <div className="flex flex-col gap-8 items-center w-full relative z-10 mt-8">
+          {/* Компонент с очками */}
           <Score user={user} />
 
+          {/* Компонент с монетами */}
           <Coin user={user} />
+
+          {/* Энергия */}
+          <EnergyBar user={user} />
         </div>
 
+        {/* Компонент друзей */}
+        <Friends user={user} />
+
+        {/* Компонент миссий */}
+        <Missions user={user} />
+
+        {/* Нижняя панель с кнопками */}
         <div className="flex flex-col gap-5 self-stretch mt-auto w-[calc(100vw-32px)] fixed bottom-8 z-10">
           <Nav />
+        </div>
 
-          {/* 🔥 Текст "BURN IT ALL" */}
-          <div className="absolute bottom-30 left-1/2 transform -translate-x-1/2 text-white text-2xl font-extrabold z-20">
-            BURN IT ALL
-          </div>
+        {/* 🔥 Размытие + картинка огня — теперь находится в самом низу */}
+        <div className="absolute bottom-0 left-0 w-full h-full bg-opacity-50">
+          <img 
+            src="/whattapcoinmin.gif" 
+            alt="Fire Background" 
+            className="w-full h-full object-cover blur-lg opacity-80"
+          />
+        </div>
 
-          {/* 🔥 Размытие + картинка огня */}
-          <div className="absolute bottom-0 left-0 w-full h-full bg-opacity-50">
-            <img 
-              src="/whattapcoinmin.gif" 
-              alt="Fire Background" 
-              className="w-full h-full object-cover blur-lg opacity-80"
-            />
-          </div>
-
+        {/* 🔥 Текст "BURN IT ALL" внизу */}
+        <div className="absolute bottom-30 left-1/2 transform -translate-x-1/2 text-white text-2xl font-extrabold z-20">
+          BURN IT ALL
         </div>
       </main>
     );
