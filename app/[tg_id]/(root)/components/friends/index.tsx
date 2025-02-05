11cb70deb4fa12ce7af55dc67c7db9d45eb0acc4
NaloZz <64098870+NaloZz@users.noreply.github.com>
2025-02-05 22:49:32 +0500

    Update index.tsx

--- app/[tg_id]/(root)/components/friends/index.tsx
+++ app/[tg_id]/(root)/components/friends/index.tsx
@@ -29,16 +29,24 @@ const Friends = ({ tg_id, friends }: { tg_id: string; friends: User[] }) => {
 		<Sheet open={sheet === "friends"}>
 			<SheetContent side="right" onOpenAutoFocus={(e) => e.preventDefault()}>
 				<main className="w-screen h-screen flex flex-col gap-10 px-4 pt-10 pb-24 relative overflow-y-auto">
-					<h1 className="text-4xl text-center font-bold tracking-tight self-stretch">
-						{!empty && <span>{friends.length}&nbsp;</span>}
-						Friends
-					</h1>
+					{/* Добавлен блок с картинкой рук над заголовком */}
+					<div className="flex flex-col items-center mx-auto gap-[18px]">
+						<Image
+							src="/handshake.png" // Убедись, что картинка "hands" доступна в нужной папке
+							alt="handshake"
+							width={160}
+							height={160}
+							className="w-[72px] h-[72px]"
+						/>
 
+						<h1 className="text-4xl text-center font-bold tracking-tight self-stretch">
+							{!empty && <span>{friends.length}&nbsp;</span>}
+							Friends
+						</h1>
+					</div>
+
+					{/* Удален блок с "Invite friends to get bonuses" */}
 					<div className="flex flex-col gap-3 relative z-10">
-						<h2 className="text-2xl text-money font-semibold tracking-tight">
-							Invite friends to get bonuses
-						</h2>
-
 						<div className="flex flex-col items-center justify-center p-4 gap-6 bg-white/10 rounded-2xl self-stretch">
 							<div className="flex items-start gap-3.5 self-stretch">
 								<CoinIcon className="w-12 h-12" />
@@ -49,7 +57,12 @@ const Friends = ({ tg_id, friends }: { tg_id: string; friends: User[] }) => {
 									<div className="flex items-end gap-1">
 										<CoinIcon className="w-4 h-4" />
 
-										<p className="text-sm leading-[15px]">
+										<p
+											className="text-sm leading-[15px]"
+											style={{
+												color: "var(--app-color)", // Применяем основной цвет для цифр
+											}}
+										>
 											<span className="text-money font-medium">
 												{non_premium_ref_reward.toLocaleString("en-US")}
 											</span>
@@ -74,7 +87,12 @@ const Friends = ({ tg_id, friends }: { tg_id: string; friends: User[] }) => {
 									<div className="flex items-end gap-1">
 										<CoinIcon className="w-4 h-4" />
 
-										<p className="text-sm leading-[15px]">
+										<p
+											className="text-sm leading-[15px]"
+											style={{
+												color: "var(--app-color)", // Применяем основной цвет для цифр
+											}}
+										>
 											<span className="text-money font-medium">
 												{premium_ref_reward.toLocaleString("en-US")}
 											</span>
