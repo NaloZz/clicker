a8498adb52a4e6405c77d527e72a9b51ab5def73
NaloZz <64098870+NaloZz@users.noreply.github.com>
2025-02-05 16:44:50 +0500

    Add files via upload

---
+++ app/[tg_id]/(root)/page.tsx
@@ -0,0 +1,81 @@
+import { cn } from "@/lib/utils";
+import prismadb from "@/lib/prismadb";
+import { getClan } from "@/lib/server-actions";
+
+import CreateButton from "@/components/ui/CreateButton";
+
+import Shop from "./components/shop";
+import Nav from "./components/main/Nav";
+import Clan from "./components/main/Clan";
+import Coin from "./components/main/Coin";
+import Friends from "./components/friends";
+import Score from "./components/main/Score";
+import Missions from "./components/missions";
+import EnergyBar from "./components/main/EnergyBar";
+
+export default async function Home({
+	params: { tg_id },
+}: { params: { tg_id: string } }) {
+	try {
+		const user = await prismadb.user.findUniqueOrThrow({
+			where: { tg_id },
+			include: { Clan: true, referrals: true },
+		});
+
+		const clan = await getClan({ tg_id: user.Clan?.tg_id });
+
+		return (
+			<main className="min-h-screen flex flex-col items-center p-4 pt-2 overflow-x-hidden relative">
+				<Clan tg_id={tg_id} clan={clan} clanId={user.clanId} />
+
+				<div className="flex flex-col gap-8 items-center w-full relative z-10 mt-8">
+					<Score user={user} />
+
+					<Coin user={user} />
+				</div>
+
+				<div className="flex flex-col gap-5 self-stretch mt-auto w-[calc(100vw-32px)] fixed bottom-8 z-10">
+					<Nav />
+
+					{/* 🔥 Текст "BURN IT ALL" (поднял выше) */}
+<div className="absolute bottom-30 left-1/2 transform -translate-x-1/2 text-white text-2xl font-extrabold z-20">
+    BURN IT ALL
+</div>
+
+					{/* 🔥 Размытие + картинка огня */}
+					<div className="absolute bottom-0 left-0 w-full h-[120px] bg-opacity-50">
+						<img 
+							src="/whattapcoinmin.gif" 
+							alt="Fire Background" 
+							className="w-full h-full object-cover blur-lg opacity-80"
+						/>
+					</div>
+
+					<EnergyBar user={user} />
+				</div>
+
+				{/* 🔥 Радужный градиентный фон */}
+				<div
+					className={cn(
+						"fixed bottom-0 left-0 w-screen h-screen z-0",
+						"[background:radial-gradient(ellipse_100%_80%_at_bottom,var(--app-color),transparent_100%)]",
+					)}
+				/>
+
+				<Shop user={user} />
+
+				<Friends tg_id={tg_id} friends={user.referrals} />
+
+				<Missions user={user} referralsNumber={user.referrals.length} />
+			</main>
+		);
+	} catch (error) {
+		console.log(error);
+
+		return (
+			<main className="min-h-screen flex items-center justify-center">
+				<CreateButton />
+			</main>
+		);
+	}
+}
---
+++ app/desktop/page.tsx
@@ -0,0 +1,10 @@
+const DesktopPage = () => {
+	return (
+		<main className="min-h-screen flex items-center justify-center">
+			You can only play ULT100x from your cell mobile.
+			Играть в ULT100x можно только с телефона.
+		</main>
+	);
+};
+
+export default DesktopPage;
