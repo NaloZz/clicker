7a6bf78f7ccd7ce1ecba71893bccc18c34d4cb4f
NaloZz <64098870+NaloZz@users.noreply.github.com>
2025-02-05 21:06:35 +0500

    Update page.tsx

--- app/[tg_id]/(root)/page.tsx
+++ app/[tg_id]/(root)/page.tsx
@@ -30,7 +30,7 @@ export default async function Home({
 				<img
 					src="https://raw.githubusercontent.com/NaloZz/clicker/refs/heads/main/public/whattapcoinmin.gif" // Замените на свой URL
 					alt="Background GIF"
-					className="absolute top-0 left-0 w-[70%] h-[70%] object-contain blur-lg opacity-40"
+					className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[80%] h-[80%] object-contain blur-sm"
 				/>
 
 				{/* Основной контент страницы */}
